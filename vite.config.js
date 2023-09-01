473042afa69d54d35bb7e452518ebd97f7079a9e
GerryIngenia <gerardo.ramirez@ingenia.com>
2023-09-01 15:21:44 -0600

    categorias

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'index.html')
+				main: resolve(__dirname, 'index.html'),
+				categorias: resolve(__dirname, 'categorias.html'),
 			},
 			output: {
 				assetFileNames: "css/styles.min[extname]",
